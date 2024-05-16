efa1ea170be72452448aa44820d2fc57e527825c
DongminLee <ldm030475@naver.com>
2024-05-16 10:59:21 +0900

    add: controller 테스트 코드 추가 turnSensorOff, rebootSensor 추가

--- src/test/java/live/ioteatime/controlservice/controller/MqttClientControllerTest.java
+++ src/test/java/live/ioteatime/controlservice/controller/MqttClientControllerTest.java
@@ -1,5 +1,6 @@
 package live.ioteatime.controlservice.controller;
 
+import live.ioteatime.controlservice.service.PayloadBuilderService;
 import live.ioteatime.controlservice.service.impl.MqttPublisherServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,14 +20,61 @@ class MqttClientControllerTest {
     MockMvc mockMvc;
     @MockBean
     MqttPublisherServiceImpl mqttPublisherService;
+    @MockBean
+    PayloadBuilderService payloadBuilderService;
 
     @Test
-    void publishMessage() throws Exception {
-        doNothing().when(mqttPublisherService).publish(anyString(), anyString());
+    void turnSensorOnTest() throws Exception {
+        String payload = "on";
+        String sensorName = "sensor";
+        String devEui = "dev";
 
-        mockMvc.perform(get("/temp/point")
-                        .contentType(MediaType.APPLICATION_JSON))
+        when(payloadBuilderService.getPayloadForTurningSensorOn(sensorName)).thenReturn(payload);
+        doNothing().when(mqttPublisherService).publish(devEui,payload);
+
+        mockMvc.perform(get("/sensor/on")
+                        .param("sensorName",sensorName)
+                        .param("devEui",devEui))
                 .andExpect(status().isOk());
+
+        verify(payloadBuilderService).getPayloadForTurningSensorOn(sensorName);
+        verify(mqttPublisherService).publish(devEui,payload);
+    }
+
+    @Test
+    void turnSensorOffTest() throws Exception {
+        String payload = "off";
+        String sensorName = "sensor";
+        String devEui = "dev";
+
+        when(payloadBuilderService.getPayloadForTurningSensorOff(sensorName)).thenReturn(payload);
+        doNothing().when(mqttPublisherService).publish(devEui,payload);
+
+        mockMvc.perform(get("/sensor/off")
+                        .param("sensorName",sensorName)
+                        .param("devEui",devEui))
+                .andExpect(status().isOk());
+
+        verify(payloadBuilderService).getPayloadForTurningSensorOff(sensorName);
+        verify(mqttPublisherService).publish(devEui,payload);
+    }
+
+    @Test
+    void rebootSensorTest() throws Exception {
+        String payload = "reboot";
+        String sensorName = "sensor";
+        String devEui = "dev";
+
+        when(payloadBuilderService.getPayloadForSensorReboot(sensorName)).thenReturn(payload);
+        doNothing().when(mqttPublisherService).publish(devEui,payload);
+
+        mockMvc.perform(get("/sensor/reboot")
+                        .param("sensorName",sensorName)
+                        .param("devEui",devEui))
+                .andExpect(status().isOk());
+
+        verify(payloadBuilderService).getPayloadForSensorReboot(sensorName);
+        verify(mqttPublisherService).publish(devEui,payload);
     }
 
 }
